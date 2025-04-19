97dafc078cc3449dea5f00478429e3044abb15ba
nidhi-builds <nidhipandit2907@gmail.com>
2025-04-19 19:46:30 +0530

    Add division.java via upload

---
+++ division.java
@@ -0,0 +1,9 @@
+public class division {
+        public static double divide(double a, double b) {
+            if (b == 0) {
+                throw new ArithmeticException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+
+}
